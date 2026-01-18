22a742f02c6e54a7af53c7f752df3800058c98b2
Termins55 <kevgame123@gmail.com>
2026-01-18 11:08:21 +0200

    Add read contacts list

---
+++ src/components/ContactsList/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+function ContactsList ({ contacts }) {
+  return (
+    <ul>
+      {contacts.map(c => (
+        <li key={c.id}>{JSON.stringify(c)}</li>
+      ))}
+    </ul>
+  );
+}
+
+const mapStateToProps = ({ contactsList }) => contactsList;
+
+export default connect(mapStateToProps)(ContactsList);
