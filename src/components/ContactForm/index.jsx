11a7798b6b956f2058ad6da44a661ff36fa585e0
Termins55 <kevgame123@gmail.com>
2026-01-18 18:05:23 +0200

    Add create contact

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { connect } from 'react-redux';
+import { CONTACT_VALIDATION_SCHEMA } from '../../utils/validationSchemas';
+import { createContact } from '../../store/slices/contactsSlice';
 
-function ContactForm() {
+function ContactForm ({createNewContact}) {
+  const initialValues = { fullName: '', phoneNumber: '' };
+
+  const submitHandler = (values, { resetForm }) => {
+createNewContact(values)
+    resetForm();
+  };
+
   return (
-    <div>ContactForm</div>
-  )
+    <Formik
+      initialValues={initialValues}
+      onSubmit={submitHandler}
+      validationSchema={CONTACT_VALIDATION_SCHEMA}
+    >
+      <Form>
+        <label>
+          Name:
+          <Field
+            name='fullName'
+            type='text'
+            placeholder='Contact name'
+            autoFocus
+          />
+          <ErrorMessage name='fullName' component='div' />
+        </label>
+        <br />
+        <label>
+          Phone
+          <Field
+            name='phoneNumber'
+            type='text'
+            maxLength='13'
+            placeholder='+XXXXXXXXXXXXX'
+          />
+          <ErrorMessage name='phoneNumber' component='div' />
+        </label>
+        <br />
+        <button type='submit'>Add</button>
+      </Form>
+    </Formik>
+  );
 }
 
-export default ContactForm
+const mapDispatchToProps = dispatch => ({
+  createNewContact: (data)=>dispatch(createContact(data))
+});
+
+export default connect(null, mapDispatchToProps)(ContactForm);
