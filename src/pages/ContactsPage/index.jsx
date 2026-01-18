1252da5ef3080ae651dc6d91dc5fba57ae721699
Termins55 <kevgame123@gmail.com>
2026-01-18 11:08:05 +0200

    Add base components structure

---
+++ src/pages/ContactsPage/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ContactForm from '../../components/ContactForm';
+import ContactsList from '../../components/ContactsList';
+
+function ContactsPage () {
+  return (
+    <main>
+      <h2>Contacts</h2>
+      <ContactForm />
+      <ContactsList />
+    </main>
+  );
+}
+
+export default ContactsPage;
